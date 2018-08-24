90951afb3c12a5f49c42b0b34d244ca472aca35a
Tai Tran <hongtai91@gmail.com>
2018-08-24 17:18:26 +0700

    Edit SQL formula

--- erpnext/crm/doctype/quick_credit_assessment_sme/quick_credit_assessment_sme.py
+++ erpnext/crm/doctype/quick_credit_assessment_sme/quick_credit_assessment_sme.py
@@ -11,26 +11,37 @@ class QuickCreditAssessmentSME(Document):
 		final_score = frappe.db.sql("""SELECT sum(a.score*a.weight) as final_score FROM
 			(
 				SELECT 'doanh_thu' as criteria, doanh_thu as criteria_mark from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL 
-				SELECT 'he_so_no_vay', he_so_no_vay from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'kinh_nghiem_gd', kinh_nghiem_gd from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'fb_kinh_doanh_nhieu_nganh', fb_kinh_doanh_nhieu_nganh from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pla_ty_trong_sx', pla_ty_trong_sx from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pla_so_nam_hoat_dong', pla_so_nam_hoat_dong from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pe_so_luong_cua_hang', pe_so_luong_cua_hang from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pha_trung_thau', pha_trung_thau from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pha_doanh_thu', pha_doanh_thu from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
 				SELECT 'loi_nhuan_sau_thue', loi_nhuan_sau_thue from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'no_qua_han_cic', no_qua_han_cic from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'fb_phan_loai_dai_ly', fb_phan_loai_dai_ly from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pla_nganh_kinh_doanh', pla_nganh_kinh_doanh from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pla_he_so_no_vay_ngan_han', pla_he_so_no_vay_ngan_han from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pe_loai_hinh_so_huu', pe_loai_hinh_so_huu from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'pha_thoi_gian_hop_tac', pha_thoi_gian_hop_tac from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
 				SELECT 'lo_luy_ke', lo_luy_ke from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
-				SELECT 'so_nam_hoat_dong', so_nam_hoat_dong from `tabQuick Credit Assessment SME` WHERE name = '{rcd}'
+				SELECT 'he_so_no_vay', he_so_no_vay from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'no_qua_han_cic', no_qua_han_cic from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'register_capital', register_capital from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'own_capital', own_capital from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'asset_guarantor_percentage', asset_guarantor_percentage from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'guarantor_liquidity', guarantor_liquidity from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'other_banks_relationship', other_banks_relationship from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'kinh_doanh_nhieu_nganh', kinh_doanh_nhieu_nganh from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'lich_su_qhtd_eib', lich_su_qhtd_eib from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'so_nam_hoat_dong', so_nam_hoat_dong from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'kinh_nghiem_gd', kinh_nghiem_gd from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'education_level_director', education_level_director from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'transaction_office_ownership', transaction_office_ownership from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pla_ty_trong_sx', pla_ty_trong_sx from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pla_loan_interest', pla_loan_interest from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pla_ty_trong_xuat_khau', pla_ty_trong_xuat_khau from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pla_nganh_kinh_doanh', pla_nganh_kinh_doanh from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pla_so_nam_hoat_dong', pla_so_nam_hoat_dong from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pla_vehicle_number', pla_vehicle_number from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'local_lc_at_local_bank', local_lc_at_local_bank from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pe_so_luong_cua_hang', pe_so_luong_cua_hang from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pe_additional_facilities', pe_additional_facilities from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'pe_total_pumper', pe_total_pumper from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'fb_loan_interest', fb_loan_interest from `tabQuick Credit Assessment SME` WHERE name = '{rcd}' UNION ALL
+				SELECT 'fb_phan_loai_dai_ly', fb_phan_loai_dai_ly from `tabQuick Credit Assessment SME` WHERE name = '{rcd}'
 			) d
 			LEFT JOIN `tabQuick Credit Assessment SME Criteria` a ON a.criteria_value = d.criteria_mark AND a.criteria_name = d.criteria
-		""".format(rcd=self.name))[0][0]
+			WHERE d.criteria_product = '{prd}'
+		""".format(rcd=self.name,prd=self.loai_nganh_nghe))[0][0]
 
 		if final_score < 10:
 			self.credit_scoring_result = "Loai ho so"
