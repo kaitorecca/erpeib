9af67732878125517ce00dcb135a46d810c27b01
Tai Tran <hongtai91@gmail.com>
2018-09-13 10:07:55 +0700

    Some commit

--- erpnext/crm/doctype/lead/lead.js
+++ erpnext/crm/doctype/lead/lead.js
@@ -33,8 +33,8 @@ erpnext.LeadController = frappe.ui.form.Controller.extend({
 			// this.frm.add_custom_button(__("Customer"), this.create_customer, __("Make"));
 			// this.frm.add_custom_button(__("Opportunity"), this.create_opportunity, __("Make"));
 			// this.frm.add_custom_button(__("Quotation"), this.make_quotation, __("Make"));
-			this.frm.add_custom_button(__("Transaction Management"), this.make_transaction_management, __("Make"));
-			this.frm.add_custom_button(__("Call Report"), this.make_call_report, __("Make"));			
+			this.frm.add_custom_button(__("Transaction Management"), this.create_transaction_management, __("Make"));
+			this.frm.add_custom_button(__("Call Report"), this.create_call_report, __("Make"));			
 			cur_frm.page.set_inner_btn_group_as_primary(__("Make"));
 		}
 
