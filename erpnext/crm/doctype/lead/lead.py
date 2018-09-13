44ed13183a290f1a21e99d6daa7fecaed642d208
Tai Tran <hongtai91@gmail.com>
2018-09-13 10:05:39 +0700

    Some commit

--- erpnext/crm/doctype/lead/lead.py
+++ erpnext/crm/doctype/lead/lead.py
@@ -1,7 +1,6 @@
+# -*- coding: utf-8 -*-
 # Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
 # License: GNU General Public License v3. See license.txt
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 
 from __future__ import unicode_literals
