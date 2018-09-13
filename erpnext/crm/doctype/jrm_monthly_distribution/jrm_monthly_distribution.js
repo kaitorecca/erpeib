b24d863909ba754dd2e004f92e1e720cc7cde784
Tai Tran <hongtai91@gmail.com>
2018-09-13 15:30:14 +0700

    Some commit

--- erpnext/crm/doctype/jrm_monthly_distribution/jrm_monthly_distribution.js
+++ erpnext/crm/doctype/jrm_monthly_distribution/jrm_monthly_distribution.js
@@ -6,3 +6,19 @@ frappe.ui.form.on('JRM Monthly Distribution', {
 
 	}
 });
+
+
+
+cur_frm.cscript.onload = function(doc,cdt,cdn){
+	if(doc.__islocal){
+		var callback1 = function(r,rt){
+			refresh_field('percentages');
+		}
+
+		return $c('runserverobj', {'method':'get_months', 'docs':doc}, callback1);
+	}
+}
+
+cur_frm.cscript.refresh = function(doc,cdt,cdn){
+	cur_frm.toggle_display('distribution_id', doc.__islocal);
+}
--- erpnext/crm/doctype/jrm_monthly_distribution/jrm_monthly_distribution.py
+++ erpnext/crm/doctype/jrm_monthly_distribution/jrm_monthly_distribution.py
@@ -7,4 +7,15 @@ import frappe
 from frappe.model.document import Document
 
 class JRMMonthlyDistribution(Document):
-	pass
+	def get_months(self):
+		month_list = ['January','February','March','April','May','June','July','August','September',
+		'October','November','December']
+		idx =1
+		for m in month_list:
+			mnth = self.append('percentages')
+			mnth.month = m
+			mnth.percentage_allocation = 100.0/12
+			mnth.idx = idx
+			idx += 1
+
+
