988a695df7495d1d4a7fa70c7e0d73aaace6da01
Tai Tran <hongtai91@gmail.com>
2018-10-16 01:11:38 +0700

    Some commit

--- erpnext/crm/doctype/transaction_management/transaction_management_list.js
+++ erpnext/crm/doctype/transaction_management/transaction_management_list.js
@@ -13,7 +13,7 @@ frappe.ui.Page = frappe.ui.Page.extend({
 		var link = $('<a>').html(label).on("click", action).appendTo(li);
 
 		if(insert_after) {
-			li.insertAfter(parent.find(insert_after));
+			li.appendTo(parent.find(insert_after));
 		} else {
 			if(prepend) {
 				li.prependTo(parent);
