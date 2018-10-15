6210a0777e2c5291e7d919f1c66ad9d5b1351c76
Tai Tran <hongtai91@gmail.com>
2018-10-15 18:07:48 +0700

    A

--- erpnext/crm/doctype/transaction_management/transaction_management_list.js
+++ erpnext/crm/doctype/transaction_management/transaction_management_list.js
@@ -20,30 +20,30 @@ frappe.listview_settings['Transaction Management'] = {
 
 		me.page.add_sidebar_item(__("Closing Next Month"), function() {
 			cur_list.filter_list.clear_filters()
-			me.filter_area.add([[me.doctype, "execution", '<=', frappe.datetime.add_months(frappe.datetime.month_end(),1)]]);
-			me.filter_area.add([[me.doctype, "execution", '>=', frappe.datetime.add_months(frappe.datetime.month_start(),1)]]);
+			cur_list.filter_list.add_filter([[me.doctype, "execution", '<=', frappe.datetime.add_months(frappe.datetime.month_end(),1)]]);
+			cur_list.filter_list.add_filter([[me.doctype, "execution", '>=', frappe.datetime.add_months(frappe.datetime.month_start(),1)]]);
 		}, ".assigned-to-me");
 
 		me.page.add_sidebar_item(__("Closing This Month"), function() {
 			cur_list.filter_list.clear_filters()
-			me.filter_area.add([[me.doctype, "execution", '<=', frappe.datetime.month_end()]]);
-			me.filter_area.add([[me.doctype, "execution", '>=', frappe.datetime.month_start()]]);			
+			cur_list.filter_list.add_filter([[me.doctype, "execution", '<=', frappe.datetime.month_end()]]);
+			cur_list.filter_list.add_filter([[me.doctype, "execution", '>=', frappe.datetime.month_start()]]);			
 		}, ".assigned-to-me");		
 
 		me.page.add_sidebar_item(__("My Transaction"), function() {
 			cur_list.filter_list.clear_filters()
-			me.filter_area.add([[me.doctype, "owner", '=', frappe.session.user]]);
+			cur_list.filter_list.add_filter([[me.doctype, "owner", '=', frappe.session.user]]);
 		}, ".assigned-to-me");
 
 		me.page.add_sidebar_item(__("New in last 7 days"), function() {
 			cur_list.filter_list.clear_filters()
-			me.filter_area.add([[me.doctype, "created_on", '>=', frappe.datetime.add_days(frappe.datetime.get_today(),-7)]]);
+			cur_list.filter_list.add_filter([[me.doctype, "created_on", '>=', frappe.datetime.add_days(frappe.datetime.get_today(),-7)]]);
 		}, ".assigned-to-me");
 
 
 		me.page.add_sidebar_item(__("Won"), function() {
 			cur_list.filter_list.clear_filters()
-			me.filter_area.add([[me.doctype, "opportunity_status", '=', "FN-Hoàn thành, đã giải ngân"]]);
+			cur_list.filter_list.add_filter([[me.doctype, "opportunity_status", '=', "FN-Hoàn thành, đã giải ngân"]]);
 		}, ".assigned-to-me");		
 
 		me.transaction_management_sidebar_setup = true;
