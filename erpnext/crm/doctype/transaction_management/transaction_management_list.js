0b53d901824e36853a157473cbdd840b9384d8ba
Tai Tran <hongtai91@gmail.com>
2018-10-16 00:58:53 +0700

    Some commit

--- erpnext/crm/doctype/transaction_management/transaction_management_list.js
+++ erpnext/crm/doctype/transaction_management/transaction_management_list.js
@@ -1,13 +1,4 @@
-frappe.listview_settings['Transaction Management'] = {
-	onload: function(me) {
-		// frappe.route_options = {
-		// 	"owner": frappe.session.user,
-		// 	"status": "Open"
-		// };
-		me.page.set_title(__("Transaction Management"));
-
-	},
-
+frappe.ui.Page = frappe.ui.Page.extend({
 	add_extra_sidebar: function(me){
 		var parent = me.sidebar.find(".sidebar-menu.standard-actions");
 		var li = '<ul class="list-unstyled sidebar-menu sidebar-choose-view"><li class="divider"></li>  <li class="h6 stat-label">Choose your view</li></ul>';
@@ -30,6 +21,19 @@ frappe.listview_settings['Transaction Management'] = {
 			}
 		}
 		return link;
+	}
+});
+
+
+
+frappe.listview_settings['Transaction Management'] = {
+	onload: function(me) {
+		// frappe.route_options = {
+		// 	"owner": frappe.session.user,
+		// 	"status": "Open"
+		// };
+		me.page.set_title(__("Transaction Management"));
+
 	},
 
 
@@ -38,43 +42,42 @@ frappe.listview_settings['Transaction Management'] = {
 		// override assigned to me by owner
 		 if (me.transaction_management_sidebar_setup) return;
 
-		 this.add_sidebar_item();
-
+		 me.page.add_extra_sidebar();
 
 		// add assigned by me
-		this.add_extra_sidebar_item(__("All Transaction"), function() {
+		me.page.add_extra_sidebar_item(__("All Transaction"), function() {
 			cur_list.filter_list.clear_filters();
 			me.run();
 		}, ".stat-label");
 
-		this.add_extra_sidebar_item(__("Closing Next Month"), function() {
+		me.page.add_extra_sidebar_item(__("Closing Next Month"), function() {
 			cur_list.filter_list.clear_filters();
 			me.filter_list.add_filter(me.doctype, "execution", '<=', frappe.datetime.add_months(frappe.datetime.month_end(),1));
 			me.filter_list.add_filter(me.doctype, "execution", '>=', frappe.datetime.add_months(frappe.datetime.month_start(),1));
 			me.run();
 		}, ".stat-label");
 
-		this.add_extra_sidebar_item(__("Closing This Month"), function() {
+		me.page.add_extra_sidebar_item(__("Closing This Month"), function() {
 			cur_list.filter_list.clear_filters()
 			me.filter_list.add_filter(me.doctype, "execution", '<=', frappe.datetime.month_end());
 			me.filter_list.add_filter(me.doctype, "execution", '>=', frappe.datetime.month_start());
 			me.run();			
 		}, ".stat-label");		
 
-		this.add_extra_sidebar_item(__("My Transaction"), function() {
+		me.page.add_extra_sidebar_item(__("My Transaction"), function() {
 			cur_list.filter_list.clear_filters();
 			me.filter_list.add_filter(me.doctype, "owner", '=', frappe.session.user);
 			me.run();
 		}, ".stat-label");
 
-		this.add_extra_sidebar_item(__("New in last 7 days"), function() {
+		me.page.add_extra_sidebar_item(__("New in last 7 days"), function() {
 			cur_list.filter_list.clear_filters();
 			me.filter_list.add_filter(me.doctype, "creation", '>=', frappe.datetime.add_days(frappe.datetime.get_today(),-7));
 			me.run();
 		}, ".stat-label");
 
 
-		this.add_extra_sidebar_item(__("Won"), function() {
+		me.page.add_extra_sidebar_item(__("Won"), function() {
 			cur_list.filter_list.clear_filters();
 			me.filter_list.add_filter(me.doctype, "opportunity_status", '=', "FN-Hoàn thành, đã giải ngân");
 			me.run();
